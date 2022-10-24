cff4282b9c7c297669ebc3ee1109f42c614f0eab
JoseGarciaS <jargarcias@unitec.edu>
2022-10-24 17:38:10 -0600

    Test

--- src/main/java/DBManagerTool.java
+++ src/main/java/DBManagerTool.java
@@ -12,5 +12,6 @@ public class DBManagerTool {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello world");
+        System.out.println("hola2")
     }
 }
