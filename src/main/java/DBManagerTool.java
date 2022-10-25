6f16eaa446d548d3b5755813205b1d05fa875aec
JoseGarciaS <jargarcias@unitec.edu>
2022-10-25 00:20:56 -0600

    Gui Implementation 1

--- src/main/java/DBManagerTool.java
+++ src/main/java/DBManagerTool.java
@@ -10,8 +10,6 @@
 public class DBManagerTool {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello world");
-        System.out.println("hola2")
+        
     }
 }
